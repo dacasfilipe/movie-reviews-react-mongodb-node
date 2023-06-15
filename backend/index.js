f3f690f281f2ab0091f5ed1d5b2f1d4d65e392da
dacasfilipe <filiperdacas@gmail.com>
2023-06-15 09:39:08 -0300

    inserido arquivo index.js

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+import app from './server';
+import mongodb from 'mongodb';
+import dotenv from 'dotenv';
+
+async function main() {
+    
+    dotenv.config();
+    const client = 
+new mongodb.MongoClient(process.env.MOVIEREVIEWS_DB_URI);
+    const port = process.env.PORT || 8000
+
+    
+}
