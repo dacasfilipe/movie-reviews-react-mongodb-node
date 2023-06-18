01920244bb9a8d73697ee82b01fe4c1ca71a314a
dacasfilipe <filiperdacas@gmail.com>
2023-06-18 15:00:14 -0300

    corrigido fix no movies.routes

--- backend/api/movies.route.js
+++ backend/api/movies.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-const router = express.router();//get acess to express router
+const router = express.Router();//get acess to express router
 router.route('/').get((req, res) => {
     res.send('hello world');
 })
