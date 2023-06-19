5a0e64ab3fd1aee4a138edf2f04c666577c2e77d
dacasfilipe <filiperdacas@gmail.com>
2023-06-19 07:39:07 -0300

    implementado método getmovies

--- backend/dao/moviesDAO.js
+++ backend/dao/moviesDAO.js
@@ -11,4 +11,32 @@ export default class MoviesDAO{
             console.error(`Unable to establish collection handles in moviesDAO: ${err}`)
         }
     }
+
+    static async getMovies({//default filter
+        filters = null,
+        page = 0,
+        moviesPerPage = 20, //will only get 20 movies per page
+    } = {}){
+      let query
+      if(filters){
+          if('title' in filters){
+              query = {$text:{$search: filters['title']}}
+          }else if("rated" in filters){
+            query = {"rated":{$eq: filters['rated']}}
+          }
+      }
+      let cursor
+      try{
+          cursor = await movies
+          .find(query)
+          .limit(moviesPerPage)
+          .skip(moviesPerPage * page)
+          const moviesList = await cursor.toArray()
+          const totalNumMovies = await movies.countDocuments(query)
+          return {moviesList, totalNumMovies}
+      }catch(e){
+          console.error(`Unable to get movies: ${e}`)
+          return {moviesList: [], totalNumMovies: 0}
+      }
+    }
 }
