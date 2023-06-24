752a79e9fff5dde064ed41f39c550c4fc3fa295f
dacasfilipe <filiperdacas@gmail.com>
2023-06-24 18:59:17 -0300

    inserido métodos no moviesDAO.js

--- backend/dao/moviesDAO.js
+++ backend/dao/moviesDAO.js
@@ -13,4 +13,36 @@ export default class MoviesDAO{
         }
 
     }
+
+    static async getMovies({
+        filters = null,
+        page=0,
+        moviesPerPage=20,
+    } = {}){
+        let query
+        if(filters){
+            if('title' in filters){
+                query = {$text: {$search: filters['title']}}
+            }else if('rated' in filters){
+                query = {'rated': {$eq: filters['rated']}}
+            }
+        }
+        let cursor
+        try{
+            cursor = await movies
+            .find(query)
+            .limit(moviesPerPage)
+            .skip(moviesPerPage * page)
+
+            const moviesList = await cursor.toArray()
+            const totalNumMovies = await movies.countDocuments(query)
+            return {moviesList, totalNumMovies}
+        }catch(e){
+            console.error(`unable to issue find command, ${e}`)
+            return {moviesList: [], totalNumMovies: 0}
+        }
+    }
+        
+    
+
 }
